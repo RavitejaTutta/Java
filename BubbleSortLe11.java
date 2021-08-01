6641ca098d30740e88046091bf2b89d6fac7b8d9
RavitejaTutta <84077779+RavitejaTutta@users.noreply.github.com>
2021-08-01 20:59:24 +0530

    Add files via upload

---
+++ BubbleSortLe11.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class BubbleSortLe11
+{    
+public static void main(String[] args)
+{  
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter array size:");
+int n=sc.nextInt();
+int arr[] =new int[n],temp=0;
+System.out.println("Enter "+n+" elements");
+for(int i=0;i<n;i++){
+arr[i]=sc.nextInt();
+}  
+System.out.println("Array Before Bubble Sort");  
+for(int i=0;i<arr.length;i++){ 
+System.out.print(arr[i]+" ");  
+}  
+System.out.println();  
+for(int i=0;i<n;i++){  
+for(int j=1;j<(n-i);j++)
+{  
+if(arr[j-1]>arr[j]){  
+temp=arr[j-1];  
+arr[j-1]=arr[j];  
+arr[j]=temp;  
+}
+}
+} 
+System.out.println("Array After Bubble Sort");
+for(int i=0;i<arr.length;i++){  
+System.out.print(arr[i]+" ");  
+}  
+}
+}
