aa003fa5e93b68fb63f2fd1a8ec16deff053404d
RavitejaTutta <84077779+RavitejaTutta@users.noreply.github.com>
2021-08-01 19:49:23 +0530

    Add files via upload

---
+++ DatatypeLe11.java
@@ -0,0 +1,24 @@
+class DatatypeLe11
+{
+static byte b;
+static short s;
+static boolean bl;
+static int i;
+static long l;
+static float f;
+static double d;
+static char c;
+static String st;
+public static void main(String args[])
+{
+System.out.println("Default vlaue of byte is:"+b);
+System.out.println("Default vlaue of short is:"+s);
+System.out.println("Default vlaue of boolean is:"+bl);
+System.out.println("Default vlaue of integer is:"+i);
+System.out.println("Default vlaue of float is:"+f);
+System.out.println("Default vlaue of long is:"+l);
+System.out.println("Default vlaue of double is:"+d);
+System.out.println("Default vlaue of char is:"+c);
+System.out.println("Default vlaue of string is:"+st);
+}
+}
